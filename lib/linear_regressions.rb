c7fb31627ed8816b83bf2aa7b6dbc4828ce95734
ireullin <cheinheng.lin@uitox.com>
2017-06-23 15:47:23 +0800

    Added Adagrad & Momentum but hasn't completed

--- lib/linear_regressions.rb
+++ lib/linear_regressions.rb
@@ -38,6 +38,7 @@ module LinearRegression
         end
     end
 
+
     class AlternatingLeastSquares < LinearRegressionBase
         def train(entries)
             label = entries.map{|e| e[:label] }
@@ -51,14 +52,14 @@ module LinearRegression
             @beta = (x.t * x).inv * x.t * y
         end
     end
-
     # alias
     ALS = AlternatingLeastSquares
 
+
     class GradientDescent < LinearRegressionBase
-        def initialize(num_iter: 100, alpha: 0.01)
+        def initialize(num_iter: 100, learning_rate: 0.01)
             @num_iter = num_iter
-            @alpha = alpha
+            @alpha = learning_rate
         end
 
         def train(entries, &block)
@@ -75,13 +76,87 @@ module LinearRegression
             @num_iter.times do |i|
                 y_bar = x * @beta
                 loss = y_bar - y
-                gradient = (x.t * loss)/ entries.size
+
+                # cost = 1/2m * (x.t * loss)**2
+                # 微分後得到下列式子
+                gradient = (x.t * loss) / entries.size
+
                 @beta = @beta - (@alpha * gradient)
                 yield i,@beta,loss if block!=nil
             end
         end
     end
-
     # alias
     GD = GradientDescent
+
+
+    class Momentum < LinearRegressionBase
+        def initialize(num_iter: 100, learning_rate: 0.1, lambda:0.0001)
+            @num_iter = num_iter
+            @alpha = learning_rate
+            @lambda = lambda
+        end
+
+        def train(entries, &block)
+            label = entries.map{|e| e[:label] }
+            features = entries.map{|e| e[:features] }.map{|e| e+[1]}
+
+            @dimension = features[0].size
+
+            y = Matrix[label].t
+            x = Matrix[*features]
+
+            @beta = Matrix[@dimension.times.map{|x|1}].t
+            last_gradient = Matrix[@dimension.times.map{|x|0}].t
+
+            @num_iter.times do |i|
+                y_bar = x * @beta
+                loss = y_bar - y
+
+
+
+                # puts "gradient=#{gradient.to_a}"
+                @beta = @beta - (@alpha * gradient) + (@lambda * last_gradient)
+                yield i,@beta,loss if block!=nil
+                last_gradient = gradient
+            end
+        end
+    end
+
+
+    class Adagrad < LinearRegressionBase
+        def initialize(num_iter: 100, learning_rate: 0.01)
+            @num_iter = num_iter
+            @alpha = learning_rate
+        end
+
+        def train(entries, &block)
+            label = entries.map{|e| e[:label] }
+            features = entries.map{|e| e[:features] }.map{|e| e+[1]}
+
+            @dimension = features[0].size
+
+            y = Matrix[label].t
+            x = Matrix[*features]
+
+            @beta = Matrix[@dimension.times.map{|x|1}].t
+
+            @num_iter.times do |i|
+                y_bar = x * @beta
+                loss = y_bar - y
+
+                # cost = 1/(2*entries.size) * (x.t * loss)**2
+                # 微分後得到下列式子
+                gradient = (x.t * loss) / entries.size
+
+                # 由於gradient在此微分後為常數
+                new_alpha = @alpha / Math.sqrt(i+1)
+                @alpha = new_alpha if new_alpha != 0
+                puts "alpha=#{@alpha}"
+
+                @beta = @beta - (@alpha * gradient)
+                yield i,@beta,loss if block!=nil
+            end
+        end
+    end
 end
